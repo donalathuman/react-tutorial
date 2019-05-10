4a5c323fc73e95bbdc7822f30055db6c71a35ba1
Donal Donohoe <donal@wearehuman.ie>
2019-05-10 15:16:18 +0100

    New commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React apps</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </div>
 )
 
